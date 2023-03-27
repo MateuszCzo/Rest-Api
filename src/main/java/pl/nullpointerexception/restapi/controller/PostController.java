568e228b8c906d891783290d7e76c3f94026dc63
Mateusz Czsonyka <mateuszczosn@gmail.com>
2023-03-27 20:57:48 +0200

    Spring Security i JWT

--- src/main/java/pl/nullpointerexception/restapi/controller/PostController.java
+++ src/main/java/pl/nullpointerexception/restapi/controller/PostController.java
@@ -1,6 +1,8 @@
 package pl.nullpointerexception.restapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pl.nullpointerexception.restapi.controller.dto.PostDto;
 import pl.nullpointerexception.restapi.model.Post;
@@ -15,7 +17,9 @@ public class PostController {
 
     private final PostService postService;
     @GetMapping("/posts")
-    public List<PostDto> getPosts(@RequestParam(required = false) Integer page, Sort.Direction sort) {
+    public List<PostDto> getPosts(@RequestParam(required = false) Integer page,
+                                  Sort.Direction sort,
+                                  @AuthenticationPrincipal UsernamePasswordAuthenticationToken user) {
         int pageNumber = page != null && page >= 0 ? page : 0;
         Sort.Direction sortDirection = sort != null ? sort : Sort.Direction.ASC;
         return PostDtoMapper.mapToPostDtos(postService.getPosts(pageNumber, sortDirection));
